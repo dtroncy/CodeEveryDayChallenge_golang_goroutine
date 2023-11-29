094d35e7afb5b0c24cc3f87741cf0cab735a12d6
Damien Troncy <damien.troncy@groupama.com>
2023-11-29 17:35:26 +0100

    test waitgroup

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var wg sync.WaitGroup
+
 func run(name string) {
+
+	defer wg.Done()
+
 	for i := 0; i < 3; i++ {
 		time.Sleep(1 * time.Second) // attendre 1 seconde
 		fmt.Println(name, " : ", i)
@@ -14,10 +20,15 @@ func run(name string) {
 
 func main() {
 	debut := time.Now()
+	wg.Add(1)
 	go run("A")
+	wg.Add(1)
 	go run("B")
-	run("C")
+	wg.Add(1)
+	go run("C")
+
+	wg.Wait()
+
 	fin := time.Now()
 	fmt.Println(fin.Sub(debut))
-
 }
